0d9ed757ba06867a19c602188aa0db2405d6ba6e
Kharristov <annawey486@gmail.com>
2024-12-04 16:13:31 +0700

    Добавьте файлы проекта.

---
+++ 5c3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int b, k; 
+    double N; 
+    cin >> b >> k >> N;
+
+    string result = "0.";
+
+ 
+    for (int i = 0; i < k; i++) {
+        N *= b;
+        int digit = static_cast<int>(N);
+        N -= digit;
+        if (digit < 10) {
+            result += char(digit + '0'); 
+        }
+        else {
+            result += char(digit - 10 + 'a'); 
+        }
+
+  
+        if (abs(N) < 1e-5) {
+            break;
+        }
+    }
+
+
+    while (result.back() == '0') {
+        result.pop_back();
+    }
+
+    if (result.back() == '.') {
+        result.pop_back();
+    }
+
+    cout << result;
+
+    return 0;
+}
+
